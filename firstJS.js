5c7a301b634eb272399b9ce82096d1b8006220f9
NV12 <naeimvhora@gmail.com>
2018-01-03 13:30:58 +0530

    changing quote in the twitter link in jQuery

--- firstJS.js
+++ firstJS.js
@@ -4,9 +4,10 @@ $(document).ready(function(){
 	var arr=["Silent hoja warna mein violent ho jaunga","Premi kadi zukyo nahi ane zukshe nahi","Char-Char bangadivali gadi lai dav","Never underastimate the power of a common man"];
 	var count=0,check=0;
 	$("button").click(function(){
-		$('#paragraph-1').text("\""+arr[++count]+"\"");
 		if(count===3)
 			count=0;
+		$('#paragraph-1').text("\""+arr[++count]+"\"");
+		
 		check=1;
 	});
 
