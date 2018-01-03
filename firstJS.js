704c0a139439aff7d68046b00027c149990a9c75
NV12 <naeimvhora@gmail.com>
2018-01-03 13:23:34 +0530

    adding final changes to the code

--- firstJS.js
+++ firstJS.js
@@ -1,12 +1,21 @@
 
 $(document).ready(function(){
 
-	var arr=["Premi kadi zukyo nahi ane zukshe nahi","Char-Char bangadivali gai lai dav","Never underastimate the power of a common man","Silent hoja warna mein violent ho jaunga"];
-	var count=0;
+	var arr=["Silent hoja warna mein violent ho jaunga","Premi kadi zukyo nahi ane zukshe nahi","Char-Char bangadivali gadi lai dav","Never underastimate the power of a common man"];
+	var count=0,check=0;
 	$("button").click(function(){
-		$('#paragraph-1').text("\""+arr[count++]+"\"");
+		$('#paragraph-1').text("\""+arr[++count]+"\"");
 		if(count===3)
 			count=0;
+		check=1;
+	});
+
+	$("a.c-1").click(function(){
+		if(check===1)
+			var newURL="https://twitter.com/share?text="+arr[count];
+		else
+			var newURL="https://twitter.com/share?text="+arr[0];
+		$(this).attr('href',newURL);
 	});
 
 });
